9e97696487e610292497112bb667126fd9e628b8
Anonyme-001 <164770736+Anonyme-00152@users.noreply.github.com>
2025-12-29 09:27:04 +0100

    Delete virus-builder-tool directory

--- virus-builder-tool/configs/obfuscation.py
+++
@@ -1,9 +0,0 @@
-
-def obfuscate_strings():
-    """Simple string obfuscation"""
-    # This would contain obfuscation logic
-    pass
-
-def deobfuscate_strings():
-    """Deobfuscate strings"""
-    pass
